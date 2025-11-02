1df33cff71d13e8a809d7b3d825b4c3178812081
Roman-Isko <roman_isko@ukr.net>
2025-11-02 13:26:36 +0200

    Remove generated swagger.json from repo and ignore it

--- src/server.js
+++ src/server.js
@@ -1,17 +1,15 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import fs from 'fs';
+import path from 'path';
+import swaggerUi from 'swagger-ui-express';
 
 import contactsRouter from './routers/Contacts.js';
 import authRouter from './routers/auth.js';
-// import testEmailRouter from './routers/testEmailRouter.js';
 
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
-/**
- * Створює та налаштовує express-сервер
- * Не запускає порт — це робиться в index.js
- */
 export default function setupServer() {
   const app = express();
 
@@ -22,7 +20,11 @@ export default function setupServer() {
   // --- Routes ---
   app.use('/api/auth', authRouter);
   app.use('/api/contacts', contactsRouter);
-  // app.use('/api/test-email', testEmailRouter);
+
+  // --- Swagger Docs Route ---
+  const swaggerPath = path.resolve('docs/swagger.json');
+  const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, 'utf-8'));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   // --- Error Handlers ---
   app.use(notFoundHandler);
